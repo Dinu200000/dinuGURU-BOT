64b4b27cf64d00002599d413fff827cbfb0792ab
Dinu200000 <132956367+Dinu200000@users.noreply.github.com>
2023-05-09 15:33:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['447710173736', 'Owner', true],
-  ['447710173736', 'Ash', true], 
+  ['+94776452195', 'Owner', true],
+  ['+94776452195', 'dinu', true], 
   [''] 
 ] //Number of owners
 
